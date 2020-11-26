30e91cbb8540574f166b3b8c6a25e06e55ed68cd
Jon Ganebski <jon.ganebski@gmail.com>
2020-11-26 10:44:56 +0900

    changed data expiration date

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 export const getExpireDate = () => {
-  // Returns first day of next month.
+  // Returns 01-01T00:00:00.000Z of next year
   const today = new Date();
-  const expireAt = new Date(`${today.getFullYear()}-${today.getMonth() + 2}`);
+  const expireAt = new Date(`${today.getFullYear() + 1}`);
   const expireAtSec = expireAt.getTime() / 1000;
   return expireAtSec;
 };
